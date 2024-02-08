db28baac10ffb0cb296e6ac40d12143649b09aad
Minamotooo <aurchichowdhury07@gmail.com>
2024-02-09 01:32:06 +0600

    subquery

--- backend/hotelSignUpRouter.js
+++ backend/hotelSignUpRouter.js
@@ -31,17 +31,16 @@ hotelSignUpRouter.post('/divisions', async (req, res) => {
   
 
   hotelSignUpRouter.post('/districts', async (req, res) => {
-    const { message } = req.body;
+    const division = req.body.firstLevel;
   
     try {
       
        console.log("Request received");
-            const result = await pool.query('SELECT name FROM divisions;');
+            const result = await pool.query('SELECT name FROM districts WHERE division_id = ( SELECT division_id FROM divisions WHERE name = $1);',[division]);
             res.json({success : true, data:result.rows}); 
             console.log(result.rows);
         
-       
-  
+      
     
     } catch (error) {
       console.error('Error loading hotel signup', error);
